9a1fbcec73302d8bd7e51b34c85bd364fa65e4d6
Ted Landauer <tedlandauer@gmail.com>
2026-04-29 12:52:41 -0700

    adding in the initial addition and exploration into firebase, npm usage, and environement variables. NOTE: nothing env related will have been commited. If it has, something went wrong

--- characterInfo.js
+++ characterInfo.js
@@ -1,3 +1,21 @@
+//import { initalizeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+
+require('dotenv').config();
+
+console.log(`hello ${process.env.HELLO}`)
+
+
+const firebaseConfig = {
+	API_KEY: ${process.env.API_KEY},
+	AUTH_DOMAIN: ${process.env.AUTH_DOMAIN},
+	PROJECT_ID: ${process.env.PROJECT_ID},
+	STORAGE_BUCKET: ${process.env.STORAGE_BUCKET},
+	MESSAGING_SENDER_ID: ${process.env.MESSAGING_SENDER_ID},
+	APP_ID: ${process.env.APP_ID},
+	MEASUREMENT_ID: ${process.env.MEASUREMENT_ID},
+};*/
+
 console.log("hello world!")
 
 //Test data
